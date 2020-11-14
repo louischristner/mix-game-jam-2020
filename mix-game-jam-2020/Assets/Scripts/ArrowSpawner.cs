a3fc40d8efb106bc8cf1657586d5924db92daab1
louischristner <l.christner@orange.fr>
2020-11-15 00:59:52 +0100

    Create Rhythm elements

---
+++ mix-game-jam-2020/Assets/Scripts/ArrowSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowSpawner : MonoBehaviour
+{
+    public float rate = 2f;
+    public GameObject arrow;
+
+    private float elapsedTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedTime = rate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime >= rate) {
+            GameObject newArrow = Instantiate(arrow, GameObject.Find("Canvas").transform, false);
+            newArrow.transform.position = transform.position;
+            elapsedTime = 0f;
+        }
+    }
+}
